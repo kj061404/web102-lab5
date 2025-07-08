932d7199937b26264174723f76596ef49d4fb36c
kj061404 <kobejordan2004@icloud.com>
2025-07-07 22:31:38 -0500

    Init project

---
+++ src/App.jsx
@@ -0,0 +1,94 @@
+import { useState, useEffect } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+import CoinInfo from "./Components/CoinInfo"
+import SideNav from "./Components/SideNav"
+
+function App() {
+  const API_KEY = import.meta.env.VITE_APP_API_KEY
+  const [list, setList] = useState(null)
+  const [filteredResults, setFilteredResults] = useState([])
+  const [searchInput, setSearchInput] = useState("")
+
+  useEffect(() => {
+    const fetchAllCoinData = async () => {
+      const response = await fetch(
+        "https://min-api.cryptocompare.com/data/all/coinlist?&api_key" 
+        + API_KEY
+      )
+      const json = await response.json()
+      setList(json)
+    }
+    fetchAllCoinData().catch(console.error)
+  }, [])
+
+  const searchItems = searchValue => {
+    setSearchInput(searchValue)
+    if (searchValue !== "") {
+      const filteredData = Object.keys(list.Data).filter((item) => 
+        Object.values(item)
+          .join("")
+          .toLowerCase()
+          .includes(searchValue.toLowerCase())
+      )
+      setFilteredResults(filteredData)
+    } else {
+      setFilteredResults(Object.keys(list.Data))
+    }
+  }
+
+  return (
+    <div className="whole-page">
+      <SideNav />
+      <div className="main-content">
+        <h1>My Crypto List</h1>
+        <input
+          type="text"
+          placeholder="Search..."
+          onChange={(inputString) => searchItems(inputString.target.value)}
+        />
+        <ul>
+          {searchInput.length > 0
+            ? filteredResults
+                .map((coin) => {
+                  const coinData = list.Data[coin]
+                  if (
+                    coinData.IsTrading &&
+                    coinData.Algorithm !== "N/A" &&
+                    coinData.ProofType !== "N/A"
+                  ) {
+                    return (
+                      <CoinInfo
+                        key={coin}
+                        image={coinData.ImageUrl}
+                        name={coinData.FullName}
+                        symbol={coinData.Symbol}
+                      />
+                    )
+                  }
+                  return null
+                })
+            : list &&
+              Object.entries(list.Data)
+                .filter(
+                  ([_, coinData]) =>
+                    coinData.IsTrading &&
+                    coinData.Algorithm !== "N/A" &&
+                    coinData.ProofType !== "N/A"
+                )
+                .slice(0, 20)
+                .map(([coin, coinData]) => (
+                  <CoinInfo
+                    key={coin}
+                    image={coinData.ImageUrl}
+                    name={coinData.FullName}
+                    symbol={coinData.Symbol}
+                  />
+                ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/SideNav.jsx
@@ -0,0 +1,11 @@
+import CryptoNews from "./CryptoNews"
+
+function SideNav() {
+    return (
+        <div className="side-nav">
+            <CryptoNews />
+        </div>
+    )
+}
+
+export default SideNav 
